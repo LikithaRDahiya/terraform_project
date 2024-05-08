cf71e513949128659725dc1cdfcaf136bca68e1b
LikithaRDahiya <158450437+LikithaRDahiya@users.noreply.github.com>
2024-05-08 07:20:53 +0530

    Create route_assoc.tf

---
+++ route_assoc.tf
@@ -0,0 +1,15 @@
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "private_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route" "nat_gateway_route" {
+  route_table_id         = aws_route_table.private_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.my_nat_gateway.id
+}
